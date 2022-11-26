f2c3db46f00f4cb20aedd71168935fbe00dac793
Sandor Gyulai <ijf.gyulais@gmail.com>
2022-11-26 10:20:49 +0000

    Initial Commit

---
+++ BreakingBadQuotes/BreakingBadQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadQuotesApp.swift
+//  BreakingBadQuotes
+//
+//  Created by Sandor Gyulai on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
